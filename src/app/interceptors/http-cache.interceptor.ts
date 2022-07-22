08742b869971b10aaa9c81c2c90851bad1b2caa4
Diego Da'Silva <smartcrashomg@gmail.com>
2022-07-22 10:44:16 -0400

    Improve `HttpCacheINterceptor`: persist cache on localStorage

--- src/app/interceptors/http-cache.interceptor.ts
+++ src/app/interceptors/http-cache.interceptor.ts
@@ -4,25 +4,43 @@ import { Observable, of, tap } from "rxjs";
 
 @Injectable()
 export class HttpCacheInterceptor implements HttpInterceptor {
-  private cache: Map<string, HttpResponse<any>> = new Map();
+  private cache: Map<string, HttpResponse<any>>
 
-  constructor() { }
+  constructor() {
+    const json = localStorage.getItem('_cache') || '{}'
+    const object = JSON.parse(json)
+
+    this.cache = new Map(Object.entries(object))
+  }
+
+  private getItem(urlWithParams: string): HttpResponse<any> | undefined {
+    return this.cache.get(urlWithParams)
+  }
+
+  private setItem(urlWithParams: string, response: HttpResponse<any>): void {
+    this.cache.set(urlWithParams, response)
+
+    const object = Object.fromEntries(this.cache.entries())
+    const json = JSON.stringify(object)
+
+    localStorage.setItem('_cache', json)
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (req.method !== 'GET') {
       return next.handle(req)
     }
 
-    const cached = this.cache.get(req.urlWithParams)
+    const cached = this.getItem(req.urlWithParams)
 
-    if (cached) return of(cached.clone())
+    if (cached) return of(new HttpResponse(cached as any).clone())
 
     return next
       .handle(req)
       .pipe(
         tap((response) => {
           if (response instanceof HttpResponse) {
-            this.cache.set(req.urlWithParams, response.clone())
+            this.setItem(req.urlWithParams, response.clone())
           }
         })
       )
